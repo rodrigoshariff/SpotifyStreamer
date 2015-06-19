8c7660f06d6bdddef4d9a5f57796dd0072ec97b6
rodrigoshariff <rodrigoshariff@gmail.com>
2015-06-19 19:56:31 -0400

    Resolve Spotify API

---
+++ app/src/main/java/MultiTargetArrayAdapter.java
@@ -0,0 +1,42 @@
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import app.com.example.android.spotifystreamer.R;
+
+/**
+ * Created by rmendoza on 6/19/2015.
+ */
+public class MultiTargetArrayAdapter extends ArrayAdapter<String>{
+    private final Context context;
+    private final String[] values;
+
+    public MultiTargetArrayAdapter(Context context, String[] artistName, String[] imageUrls) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        textView.setText(values[position]);
+        // change the icon for Windows and iPhone
+        String s = values[position];
+        if (s.startsWith("iPhone")) {
+            imageView.setImageResource(R.drawable.no);
+        } else {
+            imageView.setImageResource(R.drawable.ok);
+        }
+
+        return rowView;
+    }
+}
--- app/src/main/java/app/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/app/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -1,6 +1,8 @@
 package app.com.example.android.spotifystreamer;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,14 +17,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Image;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -34,6 +42,11 @@ import retrofit.client.Response;
 public class MainActivityFragment extends Fragment {
 
     ArrayAdapter<String> mSearchArtistAdapter;
+    List<String> artistsName = new ArrayList<String>();
+    List<String> spotifyId = new ArrayList<String>();
+    List<String> artistImageURL = new ArrayList<String>();
+    List<android.media.Image> artistImages = new ArrayList<android.media.Image>();
+
 
     public MainActivityFragment() {
     }
@@ -43,13 +56,9 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
                 String[] searchArtistArray = {
-                        "Morgan James",
-                        "Rush",
-                        "Iron Maiden",
-                        "Fleetwood Mac",
-                        "Boston",
-                        "Pink Floyd",
-                        "Styx"
+                        "https://i.scdn.co/image/8345ff2772773ece533a4dbfc29e084a8c292d24",
+                        "https://i.scdn.co/image/fa5f0fae8533c41836b9e350a3a67901128a8bba",
+                        "https://i.scdn.co/image/7d1d865fb1829effa9c7f648f57790194944ee2a"
                 };
 
         List<String> artistSearch = new ArrayList<String>(Arrays.asList(searchArtistArray));
@@ -78,7 +87,10 @@ public class MainActivityFragment extends Fragment {
                     int duration = Toast.LENGTH_SHORT;
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
-                    
+
+                    QueryArtistsFromSpotify artistSearch = new QueryArtistsFromSpotify();
+                    artistSearch.execute(editText.getText().toString());
+
                     handled = true;
                 }
                 return handled;
@@ -87,11 +99,6 @@ public class MainActivityFragment extends Fragment {
 
 
         SpotifyApi api = new SpotifyApi();
-
-// Most (but not all) of the Spotify Web API endpoints require authorisation.
-// If you know you'll only use the ones that don't require authorisation you can skip this step
-        //api.setAccessToken("myAccessToken");
-
         SpotifyService spotify = api.getService();
 
         spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
@@ -105,28 +112,55 @@ public class MainActivityFragment extends Fragment {
             }
         });
 
-        spotify.searchArtists("morgan james", new Callback<ArtistsPager>() {
-            @Override
-            public void success(ArtistsPager artist, Response response) {
-                Log.d("Artist success", artist.artists.toString());
-
-                ListView listView = (ListView) rootView.findViewById(R.id.listview_artists);
-                listView.setAdapter(mSearchArtistAdapter);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("Artist failure", error.toString());
-            }
-        });
-
-
-
-
         return rootView;
 
-
     }
+
+
+
+
+    private class QueryArtistsFromSpotify extends AsyncTask<String, Void, ArtistsPager> {
+
+        private final String LOG_TAG = QueryArtistsFromSpotify.class.getSimpleName();
+
+        @Override
+        protected ArtistsPager doInBackground(String... params) {
+
+            SpotifyApi spotifyApi = new SpotifyApi();
+            SpotifyService spotifyService = spotifyApi.getService();
+            ArtistsPager artistsPager = spotifyService.searchArtists(params[0]);
+
+            return artistsPager;
+
+        }
+
+        @Override
+        protected void onPostExecute(ArtistsPager artistsPager) {
+            //super.onPostExecute(artistsPager);
+
+            List<Artist> artistsList = artistsPager.artists.items;
+
+            for(Artist a : artistsPager.artists.items) {
+                Log.d("NAME", "-----------> " + a.name + " : ");
+
+                for (Image imgUrl : a.images ){
+                    Log.d("IMAGES", "-----------> " + imgUrl.url + " : ");
+                    if(imgUrl.width <=100){
+
+                    }
+
+                }
+
+
+            }
+
+            Log.d("Artist Search results", artistsList.toString());
+            //Iterator<Artist> iterator = artistsList.iterator();
+
+        }
+    }
+
+
 
 
 
