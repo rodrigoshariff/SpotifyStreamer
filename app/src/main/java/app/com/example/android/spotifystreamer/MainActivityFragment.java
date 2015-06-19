8a17ad36e456866191a7e8e91c993861847c2a74
Rodrigo Mendoza <rodrigoshariff@gmail.com>
2015-06-18 23:36:05 -0400

    Spotify searchArtists

--- app/src/main/java/app/com/example/android/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/app/com/example/android/spotifystreamer/MainActivityFragment.java
@@ -1,16 +1,31 @@
 package app.com.example.android.spotifystreamer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 /**
@@ -49,6 +64,71 @@ public class MainActivityFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_artists);
         listView.setAdapter(mSearchArtistAdapter);
+
+
+        EditText editText = (EditText) rootView.findViewById(R.id.searchText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView editText, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    //populate listview using spotify API
+                    Context context = getActivity();
+                    CharSequence text = "This button will populate listview" + " " + editText.getText();
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+        SpotifyApi api = new SpotifyApi();
+
+// Most (but not all) of the Spotify Web API endpoints require authorisation.
+// If you know you'll only use the ones that don't require authorisation you can skip this step
+        //api.setAccessToken("myAccessToken");
+
+        SpotifyService spotify = api.getService();
+
+        spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
+            @Override
+            public void success(Album album, Response response) {
+                Log.d("Album success", album.name);
+            }
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Album failure", error.toString());
+            }
+        });
+
+        spotify.searchArtists("morgan james", new Callback<ArtistsPager>() {
+            @Override
+            public void success(ArtistsPager artist, Response response) {
+                Log.d("Artist success", artist.artists.toString());
+
+                ListView listView = (ListView) rootView.findViewById(R.id.listview_artists);
+                listView.setAdapter(mSearchArtistAdapter);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Artist failure", error.toString());
+            }
+        });
+
+
+
+
         return rootView;
+
+
     }
+
+
+
+
 }
